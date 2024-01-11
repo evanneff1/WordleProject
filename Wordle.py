f5a72b2386d1de3c01dc70f339f964375e14c23b
evan <neff.evank@gmail.com>
2024-01-11 16:36:01 -0700

    Milestone 2 completed

--- Wordle.py
+++ Wordle.py
@@ -13,18 +13,28 @@ from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 def wordle():
 
     def enter_action(s):
-        gw.show_message("You have to implement this method.")
-        if s in FIVE_LETTER_WORDS:
-            print("Sucess")
+        row = gw.get_current_row()
+        x = 0
+        letters = []
+        while x < 5:
+            letter = gw.get_square_letter(row, x)
+            letters.append(letter)
+            x = x + 1
+        word = ''.join(letters)
+        
+        if word.lower() in FIVE_LETTER_WORDS:
+            gw.show_message('Congrats, Milestone 2 is complete!')
+        else: 
+            gw.show_message('Not in word list')
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
 
-    location = random.randint(0,(len(FIVE_LETTER_WORDS)-1))
-    word = FIVE_LETTER_WORDS[location]
+    # location = random.randint(0,(len(FIVE_LETTER_WORDS)-1))
+    # word = FIVE_LETTER_WORDS[location]
 
-    for x in range(N_COLS):
-        gw.set_square_letter(0,x,word[x].upper())
+    # for x in range(N_COLS):
+    #     gw.set_square_letter(0,x,word[x].upper())
 
 # Startup code
 
