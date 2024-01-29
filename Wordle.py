ff4730830a24a0a5412aac37d39400d4236db820
adamjkelley01 <107519126+adamjkelley01@users.noreply.github.com>
2024-01-29 14:24:13 -0700

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -1,68 +1,59 @@
-# File: Wordle.py
-
-"""
-This module is the starter file for the Wordle assignment.
-BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
-"""
-
 import random
-
+import tkinter as tk
 from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS, CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR
 
 def wordle():
-    #set solution
-    location = random.randint(0,(len(FIVE_LETTER_WORDS)-1))
+    total_guesses = 0
+    correct_guesses = 0
+    incorrect_guesses = 0
+
+    # Set solution
+    location = random.randint(0, len(FIVE_LETTER_WORDS) - 1)
     solution = FIVE_LETTER_WORDS[location].upper()
-    print(solution)
 
     def enter_action(s):
-        #set current row
-        row = gw.get_current_row()
-        
-        for x in range(0, N_ROWS):
-            x = 0
-            letters = []
-            while x < 5:
-                letter = gw.get_square_letter(row, x)
-                letters.append(letter)
-                x = x + 1
-            word = ''.join(letters)
-            
-            #If word exists
-            if word.lower() in FIVE_LETTER_WORDS:
-                for col in range(0, N_COLS):
-                    #Check for correct letters
-                    if solution[col] == gw.get_square_letter(row, col):
-                        gw.set_square_color(row, col, CORRECT_COLOR)
-                    #Check for almost correct letters
-                    elif gw.get_square_letter(row, col) in solution:
-                        gw.set_square_color(row, col, PRESENT_COLOR)
-                    #Check for missing letters
-                    else:
-                        gw.set_square_color(row, col, MISSING_COLOR)
-                
-                #If word guessed correctly
-                if solution == word.upper():
-                    gw.show_message("Congrats! You did it!")                
-                else:
-                    #Move to next row
-                    gw.set_current_row(row + 1)
+        nonlocal total_guesses, correct_guesses, incorrect_guesses
+        total_guesses += 1
 
-            else: 
-                gw.show_message('Not in word list')
+        row = gw.get_current_row()
+        letters = [gw.get_square_letter(row, i) for i in range(N_COLS)]
+        word = ''.join(letters)
+
+        if word.lower() in FIVE_LETTER_WORDS:
+            correct_word = True
+            for col in range(N_COLS):
+                if solution[col] == gw.get_square_letter(row, col):
+                    gw.set_square_color(row, col, CORRECT_COLOR)
+                elif gw.get_square_letter(row, col) in solution:
+                    gw.set_square_color(row, col, PRESENT_COLOR)
+                else:
+                    gw.set_square_color(row, col, MISSING_COLOR)
+                    correct_word = False
+
+            if correct_word:
+                gw.show_message("Congrats! You did it!")
+                correct_guesses += 1
+                update_statistics()
+                return
+            else:
+                incorrect_guesses += 1  # Increment here for whole word
+                gw.set_current_row(row + 1)
+        else:
+            gw.show_message('Not in word list')
+            incorrect_guesses += 1  # Increment here for invalid word
+
+        update_statistics()
+
+        if total_guesses == N_ROWS:
+            update_statistics("Sorry, you didn't get it right.")
+
+    def update_statistics(message=""):
+        stats_msg = f"{message}\nTotal Guesses: {total_guesses}\nCorrect Guesses: {correct_guesses}\nIncorrect Guesses: {incorrect_guesses}"
+        gw.show_message(stats_msg)
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
-
-    #MILESTONE 1 
-    # location = random.randint(0,(len(FIVE_LETTER_WORDS)-1))
-    # word = FIVE_LETTER_WORDS[location]
-
-    # for x in range(N_COLS):
-    #     gw.set_square_letter(0,x,word[x].upper())
-
-# Startup code
 
 if __name__ == "__main__":
     wordle()
