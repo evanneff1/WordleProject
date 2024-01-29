ea5f04c4c1ad35d00160dcc598458960f60eb9ad
cdawg1213 <103866953+cdawg1213@users.noreply.github.com>
2024-01-29 16:09:11 -0700

    You can now toggle between regular vs. colorblind

--- Wordle.py
+++ Wordle.py
@@ -6,20 +6,27 @@ BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
 """
 
 import random
-
 from WordleDictionary import FIVE_LETTER_WORDS
-from WordleGraphics import WordleGWindow, N_COLS, N_ROWS, CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR
+from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+import tkinter as tk
 
 def wordle():
-    #set solution
-    location = random.randint(0,(len(FIVE_LETTER_WORDS)-1))
-    solution = FIVE_LETTER_WORDS[location].upper()
-    print(solution)
+    def toggle_mode():
+        nonlocal CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR
+        if mode_button.cget("text") == "Switch to Colorblind Mode":
+            CORRECT_COLOR = "#AAAAAA"  # Change these values for Colorblind Mode
+            PRESENT_COLOR = "#BBBBBB"
+            MISSING_COLOR = "#CCCCCC"
+            mode_button.config(text="Switch to Regular Mode")
+        else:
+            CORRECT_COLOR = "#228833"  # Reset to original values for Regular Mode
+            PRESENT_COLOR = "#CCBB44"
+            MISSING_COLOR = "#999999"
+            mode_button.config(text="Switch to Colorblind Mode")
 
     def enter_action(s):
-        #set current row
         row = gw.get_current_row()
-        
+
         for x in range(0, N_ROWS):
             x = 0
             letters = []
@@ -28,41 +35,42 @@ def wordle():
                 letters.append(letter)
                 x = x + 1
             word = ''.join(letters)
-            
-            #If word exists
+
             if word.lower() in FIVE_LETTER_WORDS:
                 for col in range(0, N_COLS):
-                    #Check for correct letters
                     if solution[col] == gw.get_square_letter(row, col):
                         gw.set_square_color(row, col, CORRECT_COLOR)
-                    #Check for almost correct letters
                     elif gw.get_square_letter(row, col) in solution:
                         gw.set_square_color(row, col, PRESENT_COLOR)
-                    #Check for missing letters
                     else:
                         gw.set_square_color(row, col, MISSING_COLOR)
-                
-                #If word guessed correctly
-                if solution == word.upper():
-                    gw.show_message("Congrats! You did it!")                
-                else:
-                    #Move to next row
-                    gw.set_current_row(row + 1)
 
-            else: 
+                if solution == word.upper():
+                    gw.show_message("Congrats! You did it!")
+                else:
+                    gw.set_current_row(row + 1)
+            else:
                 gw.show_message('Not in word list')
 
+    root = tk.Tk()
+    top = tk.Toplevel(root)
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
 
-    #MILESTONE 1 
-    # location = random.randint(0,(len(FIVE_LETTER_WORDS)-1))
-    # word = FIVE_LETTER_WORDS[location]
+    mode_button = tk.Button(root, text="Switch to Colorblind Mode", command=toggle_mode)
+    mode_button.grid(row=N_ROWS + 1, columnspan=N_COLS)  # Adjust the grid position as needed
 
-    # for x in range(N_COLS):
-    #     gw.set_square_letter(0,x,word[x].upper())
+    location = random.randint(0, (len(FIVE_LETTER_WORDS) - 1))
+    solution = FIVE_LETTER_WORDS[location].upper()
+
+    CORRECT_COLOR = "#228833"
+    PRESENT_COLOR = "#CCBB44"
+    MISSING_COLOR = "#999999"
+
+    root.mainloop()
 
 # Startup code
 
 if __name__ == "__main__":
     wordle()
+
